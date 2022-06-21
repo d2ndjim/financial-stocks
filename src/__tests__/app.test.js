995c40360d3221b6bb1ff13fedadf34693f1cb99
Lekan <jimoridwanlekan@gmail.com>
2022-06-22 00:18:37 +0100

    fix linters errors

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,14 +1,14 @@
-import companyReducer, {loadCompanies} from '../redux/stocks/companySlice'
-import { loadProfile } from '../redux/stocks/profileSlice'
+import companyReducer, { loadCompanies } from '../redux/stocks/companySlice';
+import { loadProfile } from '../redux/stocks/profileSlice';
 
-it("loadCompany should return a promise", () => {
+it('loadCompany should return a promise', () => {
   expect(loadCompanies()).toEqual(expect.any(Function));
 });
 
-it("loadProfile should return a promise", () => {
+it('loadProfile should return a promise', () => {
   expect(loadProfile()).toEqual(expect.any(Function));
 });
 
-it("Expect company reducer to handle loadCompanies", () => {
+it('Expect company reducer to handle loadCompanies', () => {
   expect(companyReducer({}, [loadCompanies.fulfilled])).not.toBeNull();
 });
